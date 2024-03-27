dd472a348289751c35733b3a1c0c4fd5b7f74216
eracosta-codes <er.acosta@gmail.com>
2024-03-26 23:04:39 -0400

    Update verify_feedback.php

--- Assignment7/verify_feedback.php
+++ Assignment7/verify_feedback.php
@@ -1,5 +1,15 @@
 <?php
 
-    
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $comments = $_POST['comments'];
+    $email = $_POST['email'];
+    $rating = $_POST['rating'];
 
+    $response = array('success' => true, 'message' => 'thank you for your feedback');
+}   else {
+    $response = array('success' => false, 'message' => 'Invalid request');
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
