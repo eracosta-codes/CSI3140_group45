672e8e3df6a3565c3af88aabc9f0e069818dac59
eracosta-codes <er.acosta@gmail.com>
2024-04-08 14:21:53 -0400

    main_19.9 finished

--- lab8/main_19.9.php
+++ lab8/main_19.9.php
@@ -40,19 +40,51 @@
             $query = "INSERT INTO Urltable(url, description) VALUES ($url, $description)";
 
             if(!($database = mysqli_connect("localhost","iw3htp","password")))
-            die("<p>Could not connect to database</p>");
+            die("<p>Could not connect to database</p> </body></html>");
 
             if(!mysqli_select_db($database, "URL"))
-            die("<p>Could not open URL</p>");
+            die("<p>Could not open URL</p></body></html>");
             
             if(!($result = mysqli_query($database,$query))){
-                print("<p>Could not execute query</p>");
-                die();
+                print("<p>Could not execute query</p></body></html>");
+                die("</body></html>");
             }
             mysqli_close($database);
             die();
         }
     }
 ?>
+<?php
+    $query = "SELECT * From Urltable";
+    if(!($database = mysqli_connect("localhost","iw3htp","password")))
+        die("<p>Could not connect to database</p> </body></html>");
+
+    if(!mysqli_select_db($database, "URL"))
+        die("<p>Could not open URL</p></body></html>");
+            
+    if(!($result = mysqli_query($database,$query))){
+        print("<p>Could not execute query</p></body></html>");
+        die("</body></html>");
+    }
+?>
+<h1>Websites and their descriptions</h1>
+<table>
+    <tr>
+        <th>URL's</th>
+        <th>Description</th>
+    </tr>
+</table>
+
+<?php
+    for($counter = 0; $row = mysqli_fetch_row($result); ++$counter){
+        print("<tr>");
+        foreach($row as $key => $value)
+            print("<td>$value</td>");
+        print("</tr>");
+    }
+
+    mysqli_close($database);
+?>
+
 </body>
 </html>
