050526e68ab450e265eb230812261336feaf35d7
eracosta-codes <er.acosta@gmail.com>
2024-04-08 22:55:08 -0400

    finished the database

--- lab8/database_19.9.sql
+++ lab8/database_19.9.sql
@@ -1,13 +1,17 @@
-CREATE DATABASE URL;
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS URL;
 
-\c URL
+-- Switch to the URL database
+USE URL;
 
-CREATE TABLE Urltable(
-    url varchar(40) PRIMARY KEY,
-    description text[]
+-- Create table Urltable
+CREATE TABLE Urltable (
+    url VARCHAR(40) PRIMARY KEY,
+    description TEXT
 );
 
-INSERT INTO Urltable(url, description)
+-- Insert data into Urltable
+INSERT INTO Urltable (url, description)
 VALUES 
-    ('www.deitel.com','{"Cool site!}'),
-    ('www.php.net','{"The official PHP site"}');
+    ('www.deitel.com', 'Cool site!'),
+    ('www.php.net', 'The official PHP site');
